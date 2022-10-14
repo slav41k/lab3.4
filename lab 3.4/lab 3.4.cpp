bba8df876108b8c2a4a6fa5f873a594de3ae8066
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-10-14 14:54:33 +0300

    lab3.4

---
+++ lab 3.4/lab 3.4.cpp
@@ -0,0 +1,19 @@
+﻿	#include <iostream>
+	using namespace std;
+	int main()
+	{
+		double x; // вхідний аргумент
+		double y; // вхідний параметр
+		double R; // вхідний параметр
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		// розгалуження в повній формі
+		if ((y <= R	 && y >= x && y >= 0 && x >= 0) ||
+			(y >= -R && y <= x && y <= 0 && x <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+		cin.get();
+		return 0;
+	}
